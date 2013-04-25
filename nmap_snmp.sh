48ce1a832d90e6d7ca5e3ade9b5bb7de2a8cce80
Vahid Hedayati <vahid.hedayati@gmail.com>
2013-04-25 12:51:38 +0200

    Create nmap_snmp.sh

---
+++ nmap_snmp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##################################
+# Vahid Hedayati - April 2013 
+# OK I think this by far will be the quickest and least intensive way of finding network devices.
+# So long as you have snmp configured
+# NMAP will discover IP and pass to snmp to get hostname
+
+# Your snmp key
+key="public"
+
+for names in $(nmap -sP -n 10.200.31.0/24|grep "^Host"|awk '{print $2}'); do
+        dns_name=$(snmpget -t 2 -v 1 -c $key $names .1.3.6.1.2.1.1.1.0 2>&1| grep -v Timeout|awk '{print $5}')
+        echo $names $dns_name
+done
+
+
+
